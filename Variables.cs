552bfd804d09148d4341c13daa767e2d867c69a2
GaelFG17 <gaelfg1720@gmail.com>
2024-10-16 22:16:08 -0600

    Uso basico de variables

---
+++ Variables.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace VariablesExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaración de variables
+            int numeroEntero = 10;
+            double numeroDecimal = 20.5;
+            char caracter = 'A';
+            string cadenaTexto = "Hola, mundo!";
+            bool esVerdadero = true;
+
+            // Imprimir variables
+            Console.WriteLine("Número entero: " + numeroEntero);
+            Console.WriteLine("Número decimal: " + numeroDecimal);
+            Console.WriteLine("Carácter: " + caracter);
+            Console.WriteLine("Cadena de texto: " + cadenaTexto);
+            Console.WriteLine("Booleano: " + esVerdadero);
+        }
+    }
+}
