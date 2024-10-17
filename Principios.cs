7a206b6f12fcb7a27426529d59df2bd61f115913
GaelFG17 <gaelfg1720@gmail.com>
2024-10-16 22:14:47 -0600

    Inicio de aprendizaje en c#

---
+++ Principios.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Principios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+        }
+    }
+}
