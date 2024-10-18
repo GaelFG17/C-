d40bffbf102c26d5d73ee005648cb33a786b5334
GaelFG17 <gaelfg1720@gmail.com>
2024-10-17 22:19:39 -0600

    Ejemplo de if

---
+++ Estructuras.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = 10;
+
+        if (number > 5)
+        {
+            Console.WriteLine("Mayor que 5");
+        }
+        else
+        {
+            Console.WriteLine("Menor que 5");
+        }
+    }
+}
