2d2fd40112d24876166088394bd08847fa5d545e
GaelFG17 <gaelfg1720@gmail.com>
2024-10-17 22:22:39 -0600

    Ejercicio 2 del for

---
+++ For2.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese un número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Tabla de multiplicar del {numero}:");
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{numero} x {i} = {numero * i}");
+        }
+    }
+}
